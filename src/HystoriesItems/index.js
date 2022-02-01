67df5b1a4829c42e4dffd8c89a79909d3e42267c
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-02-01 11:10:27 -0700

    Hystorias con map

---
+++ src/HystoriesItems/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function HystoriesItem({image, name}) {
+   
+    return (
+        <div className="history__iteam">
+            <div className="container--history-image">
+                <img 
+                    className="history--image"
+                    src={image}
+                />
+            </div>
+            <span className="history__iteam--name">
+                {name.toLowerCase()}
+            </span>
+        </div>
+    );
+}
+
+export { HystoriesItem };
