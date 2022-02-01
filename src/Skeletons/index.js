1897344f17cf9343a6219fc83e2a5c627902f4b6
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-02-01 11:16:32 -0700

    skeleton

---
+++ src/Skeletons/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Skeletons() {
+    return (
+        <div className="Skeletons">
+            
+        </div>
+    )
+}
+
+export { Skeletons };
