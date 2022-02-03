1220291e0c990e723128f3c2a8a6967b5a4a4e1a
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-02-03 15:15:04 -0700

    orrecion y cambio de algunas cosas

--- src/ProfileAside/index.js
+++ src/ProfileAside/index.js
@@ -6,11 +6,13 @@ function ProfileAside() {
     return (
         <div className="aside__profile--container">
             <div className="profile--container-content">
-                <img
-                    src="https://i.ibb.co/3THph5M/Me-Profile.jpg"
-                />
+                <a>
+                    <img
+                        src="https://i.ibb.co/3THph5M/Me-Profile.jpg"
+                    />
+                </a>
                 <div className="text">
-                    <a>ciog_2</a>
+                    <a href="https://www.youtube.com/">ciog_2</a>
                     <p>Carlos Iván</p>
                 </div>
             </div>
--- src/SuggestionsAside/index.js
+++ src/SuggestionsAside/index.js
@@ -6,7 +6,13 @@ function SuggestionsAside() {
     
     return (
         <div className="aside__suggestions--container">
+            <div className="suggestions--container-title">
+                <h3>Sugerencias para ti</h3>
+                <a>Ver todo</a>
+            </div>
+            <div className="suggestions-iteams">
 
+            </div>
         </div>    
     )
 }
